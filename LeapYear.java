a9a76153158e83e7fbf9f72dada708fee569c0a2
Caleb Bridges <calebbridges97@gmail.com>
2020-06-02 00:26:59 -0400

    Leap Year

--- LeapYear.java
+++ LeapYear.java
@@ -1,9 +1,37 @@
 public class LeapYear {
 
-     public static boolean isLeapYear(String year) {
+    public static boolean isLeapYear(String year) {
+        boolean leapYr = false;
+        int yearInt = Integer.parseInt(year);
         if (year.length() > 4) {
             throw new RuntimeException("Year can not be longer than 4 digits!");
-     }
-     return true;
+        } else {
+            
+            if (isDivisibleBy4(yearInt) && !isDivisibleBy100(yearInt)) {
+                leapYr = true;
+            }  
+            else if (isDivisibleBy100(yearInt) && isDivisibleBy400(yearInt)) {
+                leapYr = true;
+            } 
+            else {
+                leapYr = false;
+            }
+        }
+        return leapYr;
+    }
+
+    // checks if yearInt is divisble by 4
+    private static boolean isDivisibleBy4(int yearInt) {
+        return yearInt % 4 == 0;
+    }
+
+    // checks if yearInt is divisble by 100
+    private static boolean isDivisibleBy100(int yearInt) {
+        return yearInt % 100 == 0;
+    }
+
+    // checks if yearInt is divisble by 400
+    private static boolean isDivisibleBy400(int yearInt) {
+        return yearInt % 400 == 0;
     }
 }
--- LeapYearTest.java
+++ LeapYearTest.java
@@ -1,16 +1,17 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class LeapYearTest {
-    
-    @Test(expected = RuntimeException.class)
 
+    @Test(expected = RuntimeException.class)
     public void whenMoreThanFourNumbersAreUsedThenAnExceptionIsThrown() {
         LeapYear.isLeapYear("12345");
     }
-
-
-        
-    
+    @Test
+    public void yearShouldBeDivisbleByFourAndDivisibleBy100And400() {
+        Assert.assertEquals(true, LeapYear.isLeapYear("1996"));
+        Assert.assertEquals(false, LeapYear.isLeapYear("2001"));
+        Assert.assertEquals(true, LeapYear.isLeapYear("2000"));
+        Assert.assertEquals(false, LeapYear.isLeapYear("1900"));
+    }
 }
